3a2e80c056bac8f3f98ba36ee40097a8cb134fdc
erythros <erythros.com>
2021-04-26 15:35:54 -0400

    Clean up
    Removed debug file write statements, unused imports, and minor formatting fixes

--- solve_4.py
+++ solve_4.py
@@ -1,11 +1,7 @@
 '''Rubik's solver functions'''
 
-import sys
-
 from cube import pieces
 from si import do_move
-
-from debug import write_log_item
 
 
 def find_solution():
@@ -14,7 +10,6 @@ def find_solution():
     while not find_mids():
         symbol()
         find_0()
-        
 
     while [pieces[18], pieces[20], pieces[24], pieces[26]].count('Y') != 4:
         tmp_count = [pieces[18], pieces[20], pieces[24], pieces[26]].count('Y')
@@ -22,18 +17,19 @@ def find_solution():
         if tmp_count == 0:
             find_b()
         elif tmp_count == 1:
-             find_a()
+            find_a()
         elif tmp_count == 2:
             if (pieces[18] == 'Y' and pieces[26] == 'Y') or \
                (pieces[20] == 'Y' and pieces[24] == 'Y'):
-                   find_d()
+                find_d()
             else:
                 find_c()
         elif tmp_count == 3:
-            return(False)
+            return False
+
         find_1()
 
-    return(True)
+    return True
 
 
 def find_mids():
@@ -41,8 +37,9 @@ def find_mids():
 
     if pieces[19] == 'Y' and pieces[21] == 'Y' and \
        pieces[23] == 'Y' and pieces[25] == 'Y':
-        return(True)
-    return(False)
+        return True
+
+    return False
 
 
 def symbol():
@@ -58,30 +55,32 @@ def symbol():
     elif pieces[19] == 'Y' and pieces[25] == 'Y':
         do_move('10')
 
+
 def find_a():
     '''Fish'''
 
     if pieces[18] == 'Y' and pieces[19] == 'Y' and pieces[21] == 'Y' and \
        pieces[23] == 'Y' and pieces[25] == 'Y':
-       do_move('10')
-    elif pieces[19] == 'Y' and pieces[20] == 'Y' and pieces[21] == 'Y' and \
-         pieces[23] == 'Y' and pieces[25] == 'Y':
-         do_move('10')
-         do_move('10')
-    elif pieces[19] == 'Y' and pieces[21] == 'Y' and pieces[23] == 'Y' and \
-         pieces[25] == 'Y' and pieces[26] == 'Y':
-         do_move('9')
+        do_move('10')
+    elif (pieces[19] == 'Y' and pieces[20] == 'Y' and pieces[21] == 'Y' and
+          pieces[23] == 'Y' and pieces[25] == 'Y'):
+        do_move('10')
+        do_move('10')
+    elif (pieces[19] == 'Y' and pieces[21] == 'Y' and pieces[23] == 'Y' and
+          pieces[25] == 'Y' and pieces[26] == 'Y'):
+        do_move('9')
+
 
 def find_b():
     '''Plus'''
 
     if pieces[9] == 'Y' and pieces[11] == 'Y':
-       do_move(9)
+        do_move(9)
     elif pieces[45] == 'Y' and pieces[47] == 'Y':
-         do_move(9)
-         do_move(9)
+        do_move(9)
+        do_move(9)
     elif pieces[33] == 'Y' and pieces[29] == 'Y':
-         do_move(10)
+        do_move(10)
 
 
 def find_c():
@@ -89,32 +88,31 @@ def find_c():
 
     if pieces[19] == 'Y' and pieces[21] == 'Y' and pieces[23] == 'Y' and \
        pieces[24] == 'Y' and pieces[25] == 'Y' and pieces[26] == 'Y':
-           do_move('10')
-    elif pieces[18] == 'Y' and pieces[19] == 'Y' and pieces[21] == 'Y' and \
-         pieces[23] == 'Y' and pieces[24] == 'Y' and pieces[25] == 'Y':
-             do_move('10')
-             do_move('10')
-    elif pieces[18] == 'Y' and pieces[19] == 'Y' and pieces[20] == 'Y' and \
-         pieces[21] == 'Y' and pieces[23] == 'Y' and pieces[25] == 'Y':
-             do_move('9')
+        do_move('10')
+    elif (pieces[18] == 'Y' and pieces[19] == 'Y' and pieces[21] == 'Y' and
+          pieces[23] == 'Y' and pieces[24] == 'Y' and pieces[25] == 'Y'):
+        do_move('10')
+        do_move('10')
+    elif (pieces[18] == 'Y' and pieces[19] == 'Y' and pieces[20] == 'Y' and
+          pieces[21] == 'Y' and pieces[23] == 'Y' and pieces[25] == 'Y'):
+        do_move('9')
 
 
 def find_d():
     '''Diamonds'''
 
     if pieces[45] == 'Y':
-       do_move('9')
+        do_move('9')
     elif pieces[33] == 'Y':
-         do_move('9')
-         do_move('9')
+        do_move('9')
+        do_move('9')
     elif pieces[36] == 'Y':
-         do_move('10')
+        do_move('10')
 
 
 def find_0():
     '''Find 0'''
 
-    write_log_item('4 -1 - 0\r')
     do_move('1')
     do_move('9')
     do_move('7')
@@ -122,10 +120,10 @@ def find_0():
     do_move('8')
     do_move('2')
 
+
 def find_1():
     '''Find 1'''
 
-    write_log_item('4 -1 - 1\r')
     do_move('7')
     do_move('9')
     do_move('8')
